6254c60a9ec4bac601a5d8d39d32619968cc50ee
Jürgen Dönitz <juergen@doenitz.org>
2016-10-23 22:27:34 +0200

    Add empty alignment plugin.

---
+++ oba-alignment/src/main/java/de/sybig/oba/server/alignment/VirtualOntology.java
@@ -0,0 +1,11 @@
+/**
+ * Created on October 23, 2016
+ * @author juergen.doenitz@bioinf.med.uni-goettingen.de
+ */
+package de.sybig.oba.server.alignment;
+
+import de.sybig.oba.server.pluginManagment.OntologyLoader;
+
+public class VirtualOntology implements OntologyLoader {
+
+}
