96910e2ca25d5d92699b17a73208a506c67bc253
Jürgen Dönitz <jdoenit@uni-goettingen.de>
2016-12-02 15:22:55 +0100

    Extract common functions to an abstract marshaller class.

---
+++ oba-alignment/src/main/java/de/sybig/oba/server/inspect/TableMarshaller.java
@@ -0,0 +1,53 @@
+package de.sybig.oba.server.inspect;
+
+import de.sybig.oba.server.OntologyHelper;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Set;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.MessageBodyWriter;
+import org.semanticweb.owlapi.model.OWLEntity;
+
+/**
+ *
+ * @author juergen.doenitz@biologie.uni-goettingen.de
+ */
+public abstract class TableMarshaller implements MessageBodyWriter<Object> {
+
+    @Override
+    public boolean isWriteable(Class<?> type, Type type1, Annotation[] antns, MediaType mt) {
+        Class<?> currentType = type;
+        while (currentType != null) {
+
+            Class[] interfaces = currentType.getInterfaces();
+            for (Class c : interfaces) {
+                if (c.equals(Set.class) || c.equals(List.class)) {
+
+                    if (type1 instanceof ParameterizedType) {
+                        ParameterizedType t = (ParameterizedType) type1;
+                        if (t.getActualTypeArguments()[0].equals(Object[].class)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+            currentType = currentType.getSuperclass();
+        }
+        return false;
+    }
+
+    protected String getLabel(Object cell) {
+        return OntologyHelper.getAnnotation((OWLEntity) cell,
+                OntologyHelper.getOntology((OWLEntity) cell),
+                "label");
+
+    }
+
+    @Override
+    public long getSize(Object t, Class<?> type, Type type1, Annotation[] antns,
+            MediaType mt) {
+        return -1;
+    }
+}
--- oba-alignment/src/main/java/de/sybig/oba/server/inspect/TextMarshaller.java
+++ oba-alignment/src/main/java/de/sybig/oba/server/inspect/TextMarshaller.java
@@ -1,20 +1,14 @@
 package de.sybig.oba.server.inspect;
 
-import de.sybig.oba.server.ObaClass;
-import de.sybig.oba.server.OntologyHelper;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 import org.semanticweb.owlapi.model.OWLClass;
 
@@ -24,35 +18,7 @@ import org.semanticweb.owlapi.model.OWLClass;
  */
 @Provider
 @Produces("text/plain")
-public class TextMarshaller implements MessageBodyWriter<Object> {
-
-    @Override
-    public boolean isWriteable(Class<?> type, Type type1, Annotation[] antns, MediaType mt) {
-       Class<?>  currentType =type ;
-        while (currentType != null) {
-
-            Class[] interfaces = currentType.getInterfaces();
-            for (Class c : interfaces) {
-                if (c.equals(Set.class) || c.equals(List.class)) {
-
-                    if (type1 instanceof ParameterizedType) {
-                        ParameterizedType t = (ParameterizedType) type1;
-                        if (t.getActualTypeArguments()[0].equals(Object[].class)) {
-                            return true;
-                        }
-                    }
-                }
-            }
-            currentType = currentType.getSuperclass();
-        }
-        return false;
-    }
-
-    @Override
-    public long getSize(Object t, Class<?> type, Type type1, Annotation[] antns,
-            MediaType mt) {
-        return -1;
-    }
+public class TextMarshaller extends TableMarshaller {
 
     @Override
     public void writeTo(Object t, Class<?> type,
@@ -66,7 +32,10 @@ public class TextMarshaller implements MessageBodyWriter<Object> {
                 Object cell = row[i];
                 sb.append(cell.toString()).append('\t');
                 if (cell instanceof OWLClass) {
-//                    OntologyHelper.
+                    String label = getLabel(cell);
+                    if (label != null) {
+                        sb.append(label).append('\t');
+                    }
                 }
             }
             sb.deleteCharAt(sb.length() - 1);
@@ -74,4 +43,5 @@ public class TextMarshaller implements MessageBodyWriter<Object> {
         }
     }
 
+    
 }
